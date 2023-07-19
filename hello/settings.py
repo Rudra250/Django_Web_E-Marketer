5b855b1c80ef95c4d8baadbe8f081f2b72528849
Rudra250 <92505236+Rudra250@users.noreply.github.com>
2023-07-19 17:31:23 +0530

    settings.py

--- hello/settings.py
+++ hello/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b0%_7ozryr*j&2!*4w4k%4yo3aew)a2p0nasr34^kbr+d6g()i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
